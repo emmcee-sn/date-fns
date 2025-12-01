27ace42e6829bbcd9f43c634dfb583f17839ce8c
mark.coffey <mark.coffey@servicenow.com>
2025-12-01 11:44:24 +0000

    Add locale export for en-XA (pseudoloc)

---
+++ src/locale/en-XA/index.ts
@@ -0,0 +1,29 @@
+import formatDistance from './_lib/formatDistance/index'
+import formatLong from './_lib/formatLong/index'
+import formatRelative from './_lib/formatRelative/index'
+import localize from './_lib/localize/index'
+import match from './_lib/match/index'
+import type { Locale } from '../types'
+
+/**
+ * @type {Locale}
+ * @category Locales
+ * @summary Pseudo locale for testing internationalization.
+ * @language English (Pseudo)
+ * @iso-639-2 eng
+ * @author Design System Team
+ */
+const locale: Locale = {
+  code: 'en-XA',
+  formatDistance: formatDistance,
+  formatLong: formatLong,
+  formatRelative: formatRelative,
+  localize: localize,
+  match: match,
+  options: {
+    weekStartsOn: 0 /* Sunday */,
+    firstWeekContainsDate: 1,
+  },
+}
+
+export default locale
